f615fee7f9975ccd7ae4885b92ae54becef336f0
SKL119 <kailingshen119@gmail.com>
2023-11-26 16:58:54 -0800

    add dut to baseline

---
+++ Baseline/mm_mult_testbench.cc
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "mm_mult.h"
+#include "hls_stream.h"
+
+unsigned globalSeed;
+
+int main(void) {
+
+  // HLS streams for communicating with the mm_mult block
+  hls::stream<a_matrix> A_in;
+  hls::stream<b_matrix> B_in;
+  hls::stream<out_matrix> Out_out;
+
+  A_MATRIX_T A;
+  B_MATRIX_T B;
+  OUT_MATRIX_T Out;
+
+  DTYPE out_ref[M][O];
+  int fail = 0;
+
+  // Initialize matrix A (M*N) with random values.
+  A_MATRIX_INIT:
+  for(int i = 0; i < M; i++){
+    for(int j = 0; j < N; j++){
+      A.a[i][j] = rand_r(&globalSeed) % 512;
+    }
+  }
+
+  A_in.write(A);
+
+  // Initialize matrix B (N*O) with random values.
+  B_MATRIX_INIT:
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < O; j++){
+      B.b[i][j] = rand_r(&globalSeed) % 512;
+    }
+  }
+  
+  B_in.write(B);
+
+  // Generate expected output of A*B.
+  DTYPE sum = 0;
+  for(int i = 0; i < M; i++){
+    for(int j = 0; j < O; j++){
+      sum = 0;
+      for(int k = 0; k < N; k++){
+        sum = sum + A.a[i][k] * B.b[k][j];
+      }
+      out_ref[i][j] = sum;
+    } 
+  }
+
+  // Perform matrix-matrix multiplication.
+  dut(A_in, B_in, Out_out);
+
+  // Check results.
+  Out = Out_out.read();
+  for(int i = 0; i<M; i++) {
+    for(int j = 0; j<O; j++) {
+      printf("%d, %d, %d, expected = %d\n", i, j, Out.out[i][j], out_ref[i][j]);
+      if(Out.out[i][j] != out_ref[i][j]) {
+        fail=1;
+      } 
+    }
+  }
+
+  if(fail==1) {
+    cout << "failed" << endl;
+  }
+  else {
+    cout << "passed" << endl;
+  }
+  return 0;
+}
+
---
+++ old_versions/old_hls_tiling/mm.prj/vivado_hls.app
@@ -0,0 +1,15 @@
+<AutoPilot:project xmlns:AutoPilot="com.autoesl.autopilot.project" projectType="C/C++" name="mm.prj" top="block_mmul">
+    <Simulation argv="">
+        <SimFlow name="csim" setup="false" optimizeCompile="true" clean="false" ldflags="" mflags=""/>
+    </Simulation>
+    <includePaths/>
+    <libraryFlag/>
+    <files>
+        <file name="../../block_mmult_testbench.cc" sc="0" tb="1" cflags=" -std=c++11 -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="block_mmult.cc" sc="0" tb="false" cflags="-std=c++11" csimflags="" blackbox="false"/>
+    </files>
+    <solutions>
+        <solution name="solution1" status=""/>
+    </solutions>
+</AutoPilot:project>
+
